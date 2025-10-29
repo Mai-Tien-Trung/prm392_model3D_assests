b0ba73638b2bab8bc88668b2ebf8fe5b74187af5
maitrung0104 <maitrung0104@gmail.com>
2025-10-29 21:52:52 +0700

    Initial commit by Mai-Tien-Trung

---
+++ android/app/src/main/kotlin/com/example/day12_login/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day12_login
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = "https://prm392-api.onrender.com/api";
+  static const String register = "$baseUrl/Auth/register";
+  static const String login = "$baseUrl/Auth/login";
+}
